f020d67426bf9f5e6f70159e8f1414fc7f83256d
miguelazevedo <1251318@isep.ipp.pt>
2026-05-14 10:48:39 +0100

    temporada e prep de episodios

---
+++ src/main/java/org/example/model/Temporada.java
@@ -0,0 +1,8 @@
+package org.example.model;
+
+public class Temporada extends Serie{
+
+    public Temporada (String titulo, String descricao, String anoFinal) {
+        super (titulo,descricao,anoFinal);
+    }
+}
