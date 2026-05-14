dc306de1d08bb21fdb43a69d95fc8a104905a3ff
miguelazevedo <1251318@isep.ipp.pt>
2026-05-14 10:41:28 +0100

    classes novas

--- src/main/java/org/example/model/Recurso.java
+++ src/main/java/org/example/model/Recurso.java
@@ -1,4 +1,14 @@
 package org.example.model;
 
-public class Recurso {
+public abstract class Recurso {
+    private String titulo;
+    private String descricao;
+    private String dataLancamento;
+
+    public Recurso(String titulo, String descricao, String dataLancamento) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.dataLancamento = dataLancamento;
+    }
 }
+
