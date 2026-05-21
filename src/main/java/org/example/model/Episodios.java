9223d6ac7a8ecb22ae23c356bda9544f0ac57c5c
marianalopesaraujo06 <marianalopesaraujo06@hotmail.com>
2026-05-21 20:28:35 +0100

    classes episódio e filme

--- src/main/java/org/example/model/Episodios.java
+++ src/main/java/org/example/model/Episodios.java
@@ -1,15 +1,67 @@
 package org.example.model;
+import org.example.exceptions.JaVistoException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Episodios {
+public class Episodios implements MarcavelComoVisto, Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String titulo;
+    private List<Classificacao> classificacoes;
+    private List<Espectador> espectadoresQueViram;
 
-    public Episodios(String titulo){
-        this.titulo=titulo;
 
+    public Episodios(String titulo) {
+        this.titulo = titulo;
+        this.classificacoes = new ArrayList<>();
+        this.espectadoresQueViram = new ArrayList<>();
     }
-    public String getTitulo(){
+
+    public String getTitulo() {
         return titulo;
     }
 
+    public void adicionarClassificacao(Classificacao c) {
+        classificacoes.add(c);
+    }
 
+    public List<Classificacao> getClassificacoes() {
+        return classificacoes;
+    }
+
+    public double getClassificacaoMedia() {
+        if (classificacoes.isEmpty()) return 0.0;
+        double soma = 0;
+        for (Classificacao c : classificacoes) soma += c.getNota();
+        return soma / classificacoes.size();
+    }
+
+    public boolean temClassificacaoDe(Espectador espectador) {
+        for (Classificacao c : classificacoes) {
+            if (c.getEspectador().equals(espectador)) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isVisto(Espectador espectador) {
+        return espectadoresQueViram.contains(espectador);
+    }
+
+
+    @Override
+    public void marcarComoVisto(Espectador espectador) throws JaVistoException {
+        if (espectadoresQueViram.contains(espectador)) {
+            throw new JaVistoException(
+                    "O episódio \"" + titulo + "\" já foi marcado como visto por " + espectador.getNome() + ".");
+        }
+        espectadoresQueViram.add(espectador);
+        espectador.adicionarVisto(this);
+    }
+
+    @Override
+    public String toString() {
+        return titulo;
+    }
 }
--- src/main/java/org/example/model/Filme.java
+++ src/main/java/org/example/model/Filme.java
@@ -1,29 +1,58 @@
 package org.example.model;
+import org.example.exceptions.JaVistoException;
+import java.util.ArrayList;
+import java.util.List;
 
-import java.util.Objects;
+public class Filme extends Recurso implements MarcavelComoVisto {
 
-public class Filme extends Recurso {
     private String duracao;
+    private List<Classificacao> classificacoes;
+    private List<Espectador> espectadoresQueViram;
 
     public Filme(String titulo, String descricao, String dataLancamento, String duracao) {
         super(titulo, descricao, dataLancamento);
         this.duracao = duracao;
+        this.classificacoes = new ArrayList<>();
+        this.espectadoresQueViram = new ArrayList<>();
+    }
+
+    public void adicionarClassificacao(Classificacao c) {
+        classificacoes.add(c);
+    }
+
+
+    public List<Classificacao> getClassificacoes() {
+        return classificacoes;
+    }
+
+    public double getClassificacaoMedia() {
+        if (classificacoes.isEmpty()) return 0.0;
+        double soma = 0;
+        for (Classificacao c : classificacoes) soma += c.getNota();
+        return soma / classificacoes.size();
+    }
+
+    public boolean temClassificacaoDe(Espectador espectador) {
+        for (Classificacao c : classificacoes) {
+            if (c.getEspectador().equals(espectador)) return true;
+        }
+        return false;
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        Filme filme = (Filme) o;
-        return Objects.equals(duracao, filme.duracao);
+    public boolean isVisto(Espectador espectador) {
+        return espectadoresQueViram.contains(espectador);
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), duracao);
+    public void marcarComoVisto(Espectador espectador) throws JaVistoException {
+        if (espectadoresQueViram.contains(espectador)) {
+            throw new JaVistoException(
+                    "O filme \"" + getTitulo() + "\" já foi marcado como visto por " + espectador.getNome() + ".");
+        }
+        espectadoresQueViram.add(espectador);
+        espectador.adicionarVisto(this);
     }
-
 
     @Override
     public String toString() {
